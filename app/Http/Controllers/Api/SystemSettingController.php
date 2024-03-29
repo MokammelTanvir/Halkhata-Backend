7075d00a8a9b21a17dd247fc0e55612441128d98
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-29 21:44:47 +0600

    system setting api done

--- app/Http/Controllers/Api/SystemSettingController.php
+++ app/Http/Controllers/Api/SystemSettingController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Repositories\SystemSetting\SystemSettingInterface;
+use App\Trait\ApiResponse;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateSystemSettingRequest;
+use App\Repositories\SystemSetting\SystemSettingInterface;
 
 class SystemSettingController extends Controller
 {
+    use ApiResponse;
+
     private $systemSettingRepository;
 
     public function __construct(SystemSettingInterface $systemSettingRepository)
@@ -20,39 +24,24 @@ class SystemSettingController extends Controller
     public function index()
     {
         $data = $this->systemSettingRepository->all();
-        return $data;
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        if(!$data){
+            return $this->ResponseError([],null, 'No data found!', 200, 'error');
+        }
+        return $this->ResponseSuccess($data);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateSystemSettingRequest $request, string $id)
     {
-        //
+        try {
+            $data = $this->systemSettingRepository->update($request, $id);
+            return $this->ResponseSuccess($data, null, 'Update Successfully!');
+        } catch (\Exception $e) {
+            return $this->ResponseError($e->getMessage());
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 
 }
