bde9ff9af4ca109f40e8cd3881fe03de7b15654f
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-04-05 01:52:33 +0600

    Category Done

---
+++ app/service/FileUploadService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Support\Facades\Storage;
+
+class FileUploadService
+{
+    public function imageUpload($request, $model, $path='public/uploads')
+    {
+        if($request->file !=null){
+            $uploaded_file = $request->file;
+            /** check if old file exist */
+            if($model->file !=null){
+                Storage::delete($model->file);
+            }
+
+            $file_upload_url = Storage::putFileAs($path, $uploaded_file, $model->id.'.jpg', 'public');
+            return Storage::url($file_upload_url);
+        }
+        return;
+
+    }
+
+    public function fileUpload($request, $model, $path='public/uploads')
+    {
+        if($request->file !=null){
+            $uploaded_file = $request->file;
+            $file_extension = $uploaded_file->getClientOriginalExtension();
+
+            /** check if old file exist */
+            if($model->file !=null){
+                Storage::delete($model->file);
+            }
+
+            $file_upload_url = Storage::putFileAs($path, $uploaded_file, $model->id.'.'.$file_extension, 'public');
+            return Storage::url($file_upload_url);
+        }
+        return;
+
+    }
+
+}
+
+
